0863cf938f31ce82e61364efe7e06edda31117f7
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-09-14 11:18:52 -0600

    modifica directorios

--- app.py
+++ app.py
@@ -16,35 +16,35 @@ st.set_page_config(
 
 @st.cache_data
 def get_info():
-    sectores = pd.read_excel("../sectorial/sectorial_pib_v2.xlsx", dtype={"sector":str})
+    sectores = pd.read_excel("data/sectorial/sectorial_pib_v2.xlsx", dtype={"sector":str})
     sectores = sectores[["sector","desc"]]
     sectores_prioritarios = ["111", "112", "311", "314", "321", "322", "325", "326", "327",
                         "331", "332", "333", "334", "335", "336", "337", "339"]
     sectores = sectores[sectores.sector.isin(sectores_prioritarios)]
     
-    pib = pd.read_excel("../sectorial/sectorial_pib_v2.xlsx").fillna(0)
+    pib = pd.read_excel("data/sectorial/sectorial_pib_v2.xlsx").fillna(0)
 
-    xs = pd.read_excel("../sectorial/sectorial_xs_v2.xlsx")
+    xs = pd.read_excel("data/sectorial/sectorial_xs_v2.xlsx")
     xs = xs.merge(sectores, on="sector", how = "right").fillna(0)
 
-    ied = pd.read_excel("../sectorial/sectorial_ied_v2.xlsx", dtype={"sector":str})
+    ied = pd.read_excel("data/sectorial/sectorial_ied_v2.xlsx", dtype={"sector":str})
     ied = ied.merge(sectores, on="sector")
 
-    ied_estatal = pd.read_excel("../estatal/estatales_ied.xlsx")
+    ied_estatal = pd.read_excel("data/estatal/estatales_ied.xlsx")
     ied_estatal = ied_estatal.merge(sectores, on="sector")
 
-    estatal = pd.read_excel("../estatal/estatales_pib.xlsx").fillna(0)
+    estatal = pd.read_excel("data/estatal/estatales_pib.xlsx").fillna(0)
     estatal["sector"] =  estatal["sector"].apply(lambda x: x.replace("-",""))
     estatal = estatal.merge(sectores, on="sector", how="left").drop(columns={"desc_x"}).rename(columns={"desc_y": "desc"})
 
-    xs_estatal = pd.read_excel("../estatal/estatales_xs.xlsx")
+    xs_estatal = pd.read_excel("data/estatal/estatales_xs.xlsx")
     xs_estatal = xs_estatal.merge(sectores, on="sector", how="left")
 
-    with open('./data/mx_geojson.json', 'r') as openfile:
+    with open('data/mx_geojson.json', 'r') as openfile:
         # Reading from json file
         json_object = json.load(openfile)
     
-    vaemg = pd.read_excel("../sectorial/sectorial_vaemg_v2.xlsx", dtype={"sector":str})
+    vaemg = pd.read_excel("data/sectorial/sectorial_vaemg_v2.xlsx", dtype={"sector":str})
     vaemg =vaemg.merge(sectores, on="sector")
 
     return [pib, xs, ied, ied_estatal, sectores, estatal, xs_estatal, json_object, vaemg]
